a0cd30a0651d84db18a83a8e38c8d81643f11e9d
kabal <kabal@MARSEL>
2025-12-27 01:11:37 +0300

    fix

--- ExamAvalonia/SideBar/SideBar.axaml.cs
+++ ExamAvalonia/SideBar/SideBar.axaml.cs
@@ -1,38 +1,58 @@
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
+using Avalonia.Media;
 using ExamAvalonia.Pages;
 namespace ExamAvalonia;
 
 public partial class SideBar : UserControl
 {
+    private Button _activeButton;
+
     public SideBar()
     {
         InitializeComponent();
     }
 
-    private void Products_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    private void SetActive(Button btn)
+    {
+        // —брос предыдущей кнопки
+        if (_activeButton != null)
+            _activeButton.Background = Brushes.Gray;
+
+        _activeButton = btn;
+        _activeButton.Background = Brushes.DodgerBlue;
+        _activeButton.Foreground = Brushes.White;
+    }
+
+    private void Products_Click(object? sender, RoutedEventArgs e)
     {
         MainWindow.Instance.MainControl.Content = new Products();
+        SetActive(ProductsBtn);
     }
 
-    private void Warehouses_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    private void Warehouses_Click(object? sender, RoutedEventArgs e)
     {
         MainWindow.Instance.MainControl.Content = new WarehousesAndZones();
+        SetActive(WarehousesBtn);
     }
 
-    private void Suppliers_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    private void Suppliers_Click(object? sender, RoutedEventArgs e)
     {
         MainWindow.Instance.MainControl.Content = new Suppliers();
+        SetActive(SuppliersBtn);
     }
 
-    private void Deliveries_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    private void Deliveries_Click(object? sender, RoutedEventArgs e)
     {
         MainWindow.Instance.MainControl.Content = new DeliveriesPage();
+        SetActive(DeliveriesBtn);
     }
 
-    private void Shipments_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    private void Shipments_Click(object? sender, RoutedEventArgs e)
     {
         MainWindow.Instance.MainControl.Content = new ShipmentsPage();
+        SetActive(ShipmentsBtn);
     }
 }
